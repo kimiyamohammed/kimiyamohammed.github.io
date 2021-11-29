a444a12a07fa3d7f530b2bd8446e84c68a4aa7bf
kimiyamohammed <mamekimo@gmail.com>
2021-11-28 20:50:46 -0600

    final week-1

---
+++ Lab5/script.js
@@ -0,0 +1,21 @@
+//Question 1:
+function sum(arr){
+    let filtered = arr.filter(n => n > 20);
+    let result = filtered.reduce((accum, num) => accum + num);
+    return result;
+}
+
+//Question2:
+const getNewArray = function(arr){
+   return arr.filter(s=> s.length >= 5 && s.includes('a'))
+}
+
+
+
+let a = [11,23,32,4,5,21];
+let result1 = sum(a);
+
+let str = ['apples', 'animal', 'welcome', 'rainbow', 'universe'];
+let result2 = getNewArray(str);
+
+alert('1.Sum= ' + result1 +'\n2.NewArray= ['+ result2 +']');
